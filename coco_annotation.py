b445f9cbd4945a13a732e4f1c32c7a77716cf64f
Henu_ykc <30863407+henuykc@users.noreply.github.com>
2019-08-11 20:51:53 +0800

    Update coco_annotation.py

--- coco_annotation.py
+++ coco_annotation.py
@@ -1,52 +1,67 @@
 import json
 from collections import defaultdict
+import argparse
+import sys
 
-name_box_id = defaultdict(list)
-id_name = dict()
-f = open(
-    "mscoco2017/annotations/instances_train2017.json",
-    encoding='utf-8')
-data = json.load(f)
 
-annotations = data['annotations']
-for ant in annotations:
-    id = ant['image_id']
-    name = 'mscoco2017/train2017/%012d.jpg' % id
-    cat = ant['category_id']
 
-    if cat >= 1 and cat <= 11:
-        cat = cat - 1
-    elif cat >= 13 and cat <= 25:
-        cat = cat - 2
-    elif cat >= 27 and cat <= 28:
-        cat = cat - 3
-    elif cat >= 31 and cat <= 44:
-        cat = cat - 5
-    elif cat >= 46 and cat <= 65:
-        cat = cat - 6
-    elif cat == 67:
-        cat = cat - 7
-    elif cat == 70:
-        cat = cat - 9
-    elif cat >= 72 and cat <= 82:
-        cat = cat - 10
-    elif cat >= 84 and cat <= 90:
-        cat = cat - 11
+def parse_arguements(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, help='', default='/dataset/mscoco_2017/annotations/instances_val2017.json')
+    parser.add_argument('--output_dir', type=str, help='', default='./output')
+    parser.add_argument('--image_dir',type=str,help='',default='/dataset/mscoco_2017/val2017/%012d.jpg')
+    return parser.parse_args(argv)
 
-    name_box_id[name].append([ant['bbox'], cat])
 
-f = open('train.txt', 'w')
-for key in name_box_id.keys():
-    f.write(key)
-    box_infos = name_box_id[key]
-    for info in box_infos:
-        x_min = int(info[0][0])
-        y_min = int(info[0][1])
-        x_max = x_min + int(info[0][2])
-        y_max = y_min + int(info[0][3])
 
-        box_info = " %d,%d,%d,%d,%d" % (
-            x_min, y_min, x_max, y_max, int(info[1]))
-        f.write(box_info)
-    f.write('\n')
-f.close()
+def main(args):
+    name_box_id = defaultdict(list)
+    id_name = dict()
+    f = open(args.data_dir,encoding='utf-8')
+    data = json.load(f)
+
+    annotations = data['annotations']
+    for ant in annotations:
+        id = ant['image_id']
+        name = args.image_dir % id
+        cat = ant['category_id']
+
+        if cat >= 1 and cat <= 11:
+            cat = cat - 1
+        elif cat >= 13 and cat <= 25:
+            cat = cat - 2
+        elif cat >= 27 and cat <= 28:
+            cat = cat - 3
+        elif cat >= 31 and cat <= 44:
+            cat = cat - 5
+        elif cat >= 46 and cat <= 65:
+            cat = cat - 6
+        elif cat == 67:
+            cat = cat - 7
+        elif cat == 70:
+            cat = cat - 9
+        elif cat >= 72 and cat <= 82:
+            cat = cat - 10
+        elif cat >= 84 and cat <= 90:
+            cat = cat - 11
+
+        name_box_id[name].append([ant['bbox'], cat])
+
+    f = open('train.txt', 'w')
+    for key in name_box_id.keys():
+        f.write(key)
+        box_infos = name_box_id[key]
+        for info in box_infos:
+            x_min = int(info[0][0])
+            y_min = int(info[0][1])
+            x_max = x_min + int(info[0][2])
+            y_max = y_min + int(info[0][3])
+
+            box_info = " %d,%d,%d,%d,%d" % (
+                x_min, y_min, x_max, y_max, int(info[1]))
+            f.write(box_info)
+        f.write('\n')
+    f.close()
+
+if __name__ == '__main__':
+    main(parse_arguements(sys.argv[1:]))
