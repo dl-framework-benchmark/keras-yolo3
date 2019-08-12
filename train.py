9e5663601454f7b437feabccf96c789df75b2f39
Henu_ykc <30863407+henuykc@users.noreply.github.com>
2019-08-12 17:17:01 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,34 +7,79 @@ import keras.backend as K
 from keras.layers import Input, Lambda
 from keras.models import Model
 from keras.optimizers import Adam
-from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, Callback
+from keras.utils import multi_gpu_model
 
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
+import time
+import argparse
+import sys
 
 
-def _main():
-    annotation_path = 'train.txt'
-    log_dir = 'logs/000/'
-    classes_path = 'model_data/voc_classes.txt'
-    anchors_path = 'model_data/yolo_anchors.txt'
+
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+session = tf.Session(config=config)
+KTF.set_session(session)
+
+
+class TimePerBatch(Callback):
+    def on_train_begin(self, logs={}):
+        self.start = time.time()
+        print("=========== :",self.start)
+
+    def on_batch_end(self, batch, logs={}):
+        self.time_elapse = time.time() - self.start
+        print("Time per batch: ", self.time_elapse)
+
+
+
+
+def parse_arguements(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--annotation_path', type=str, help='', default='train.txt')
+    parser.add_argument('--output_dir', type=str, help='', default='logs/000/')
+    parser.add_argument('--classes_path',type=str,help='',default='model_data/coco_classes.txt')
+    parser.add_argument('--anchors_path', type=str, help='', default='model_data/yolo_anchors.txt')
+    parser.add_argument('--gpus', type=int, help='', default=1)
+    parser.add_argument('--batch_size_freeze', type=int, help='', default=32)
+    parser.add_argument('--batch_size_unfreeze', type=int, help='', default=8)
+    parser.add_argument('--model_path', type=str, help='', default='model_data/yolo_weights.h5')
+    return parser.parse_args(argv)
+
+
+def _main(args):
+    annotation_path = args.annotation_path
+    log_dir = args.output_dir
+    classes_path = args.classes_path
+    anchors_path = args.anchors_path
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
+    gpus=args.gpus
 
     input_shape = (416,416) # multiple of 32, hw
 
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
         model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+            freeze_body=2, weights_path=args.model_path,gpu_num=gpus)
+
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path=args.model_path,gpu_num=gpus) # make sure you know what you freeze
+
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
         monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+    time_log = TimePerBatch()
+
+
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
@@ -54,15 +99,15 @@ def _main():
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        batch_size = 32
+        batch_size = args.batch_size_freeze
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=50,
+                epochs=1,
                 initial_epoch=0,
-                callbacks=[logging, checkpoint])
+                callbacks=[logging, checkpoint, time_log])
         model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
     # Unfreeze and continue training, to fine-tune.
@@ -73,15 +118,19 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 32 # note that more GPU memory is required after unfreezing the body
+        batch_size = args.batch_size_unfreeze # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
+        start_time = time.time()
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=100,
-            initial_epoch=50,
+            epochs=2,
+            initial_epoch=1,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+        end_time = time.time()
+        print("total images/sec: %.2f" % (batch_size * max(1, num_train // batch_size) / (end_time - start_time)))
+
         model.save_weights(log_dir + 'trained_weights_final.h5')
 
     # Further training if needed.
@@ -103,7 +152,7 @@ def get_anchors(anchors_path):
 
 
 def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
-            weights_path='model_data/yolo_weights.h5'):
+            weights_path='model_data/yolo_weights.h5',gpu_num=1):
     '''create the training model'''
     K.clear_session() # get a new session
     image_input = Input(shape=(None, None, 3))
@@ -125,15 +174,20 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
             for i in range(num): model_body.layers[i].trainable = False
             print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
 
+    if gpu_num >= 2:
+        model_body = multi_gpu_model(model_body, gpus=gpu_num)
+
     model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5})(
         [*model_body.output, *y_true])
+
+
     model = Model([model_body.input, *y_true], model_loss)
 
     return model
 
 def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
-            weights_path='model_data/tiny_yolo_weights.h5'):
+            weights_path='model_data/tiny_yolo_weights.h5',gpu_num=1):
     '''create the training model, for Tiny YOLOv3'''
     K.clear_session() # get a new session
     image_input = Input(shape=(None, None, 3))
@@ -155,9 +209,13 @@ def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, f
             for i in range(num): model_body.layers[i].trainable = False
             print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
 
+    if gpu_num >= 2:
+            model_body = multi_gpu_model(model_body, gpus=gpu_num)
+
     model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.7})(
         [*model_body.output, *y_true])
+
     model = Model([model_body.input, *y_true], model_loss)
 
     return model
@@ -187,4 +245,4 @@ def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, n
     return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
 
 if __name__ == '__main__':
-    _main()
+    _main(parse_arguements(sys.argv[1:]))
